7eb19239434a26754465487384ef6239c644ae1b
Vitinho 21 <vbluis.play@gmail.com>
2020-06-24 12:22:56 -0300

    Arquivo no Xcode criado (branch master)

---
+++ Projeto no Xcode/Quoti/Quoti/Storyboard de Vitinho/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Quoti
+//
+//  Created by Vítor Bárrios Luís de Albuquerque on 24/06/20.
+//  Copyright © 2020 Vítor Bárrios Luís de Albuquerque. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Projeto no Xcode/Quoti/Quoti/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Quoti
+//
+//  Created by Vítor Bárrios Luís de Albuquerque on 23/06/20.
+//  Copyright © 2020 Vítor Bárrios Luís de Albuquerque. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
